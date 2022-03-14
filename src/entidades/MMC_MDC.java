8d2bcd099795d4befd27919233dbfd41165cf332
Leonildo Filgueira <leonildolf_12@hotmail.com>
2022-03-13 22:45:07 -0300

    adicionando metodo MDC

--- src/entidades/MMC_MDC.java
+++ src/entidades/MMC_MDC.java
@@ -17,14 +17,17 @@ public class MMC_MDC {
     private int[] validaNumeros(int[] numeros) {
         for (int i = 0; i < numeros.length; i++) {
             if (numeros[i] < 0) {
-                numeros[i] = numeros[i] * -1 ;
+                numeros[i] = numeros[i] * -1;
+            }
+            if (numeros[i] == 0) {
+                numeros[i] = 1;
             }
         }
         return numeros;
     }
 
     public int calculaMMC(int[] numeros) {
-        int [] numerosValidados = validaNumeros(numeros);
+        int[] numerosValidados = validaNumeros(numeros);
         int divisorAtual = 2;
         int MMC = 1;
         boolean MMCNaoCalculado = true;
@@ -32,7 +35,7 @@ public class MMC_MDC {
             boolean houveOperacao = false;
             for (int posicaoAtual = 0; posicaoAtual < numerosValidados.length; posicaoAtual++) {
                 if (numerosValidados[posicaoAtual] % divisorAtual == 0) {
-                    numerosValidados[posicaoAtual] = numerosValidados[posicaoAtual] / divisorAtual;
+                    numerosValidados[posicaoAtual] /= divisorAtual;
                     houveOperacao = true;
                 }
             }
@@ -44,5 +47,33 @@ public class MMC_MDC {
             MMCNaoCalculado = statusCalculoMMC(numerosValidados);
         }
         return MMC;
+    }
+
+    public int caluculaMDC(int[] numeros) {
+        int[] numerosValidados = validaNumeros(numeros);
+        int divisorAtual = 2;
+        int MDC = 1;
+        boolean MDCNaoCalculado = true;
+        while (MDCNaoCalculado) {
+            boolean houveOperacao = false;
+            int qtdDeDivisores = 0;
+            for (int posicaoAtual = 0; posicaoAtual < numerosValidados.length; posicaoAtual++) {
+                if (numerosValidados[posicaoAtual] % divisorAtual == 0) {
+                    numerosValidados[posicaoAtual] /= divisorAtual;
+                    qtdDeDivisores++;
+                    houveOperacao = true;
+                }
+                if (numerosValidados[posicaoAtual] == 1) {
+                    MDCNaoCalculado = false;
+                }
+            }
+            if (qtdDeDivisores == numerosValidados.length) {
+                MDC *= divisorAtual;
+            }
+            if (houveOperacao == false) {
+                divisorAtual++;
+            }
+        }
+        return MDC;
     }
 }
--- src/programa/Aplicacao.java
+++ src/programa/Aplicacao.java
@@ -8,25 +8,30 @@ public class Aplicacao {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println();
-        System.out.println("Calculo de MMC. Valores negativos serao considerados como positivos (regra MMC).");
+        System.out.println("Calculo de MMC. Valores negativos serao considerados como positivos (regra MMC). O valor zero não sera considerado.");
         System.out.println();
         System.out.print("Quantos numeros para calcular o MMC e MDC? ");
         int qtdNumeros = scanner.nextInt();
-        int[] numeros = new int[qtdNumeros];
-        for (int i = 0; i < numeros.length; i++) {
+        int[] numerosMMC = new int[qtdNumeros];
+        int[] numerosMDC = new int[qtdNumeros];
+
+        for (int i = 0; i < numerosMMC.length; i++) {
             System.out.print("digite o numero #" + (i + 1) + " = ");
-            numeros[i] = scanner.nextInt();
+            numerosMMC[i] = scanner.nextInt();
+            numerosMDC[i] = numerosMMC[i];
         }
+
         System.out.println();
         System.out.println("numeros digitados: ");
-        for (int i = 0; i < numeros.length; i++) {
-            System.out.print(numeros[i] + "  ");
+        for (int i = 0; i < numerosMMC.length; i++) {
+            System.out.println(numerosMMC[i] + "  ");
         }
         System.out.println();
         MMC_MDC mmc_mdc = new MMC_MDC();
-        int resultadoMMC = mmc_mdc.calculaMMC(numeros);
+        int resultadoMMC = mmc_mdc.calculaMMC(numerosMMC);
         System.out.println("MMC: " + resultadoMMC);
-        System.out.println("Calculo MDC em breve!");
+        int resultadoMDC = mmc_mdc.caluculaMDC(numerosMDC);
+        System.out.println("MDC: " + resultadoMDC);
         scanner.close();
     }
 }
